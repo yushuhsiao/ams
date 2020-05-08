a4b1cfeba52c69d34ac605cc861ed04742993504
Yushu Hsiao <yushuhsiao@gmail.com>
2020-05-08 08:10:24 +0800

    docker

---
+++ lib/Common/Defines/LogType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GLT
+{
+    /// <summary>
+    /// 交易類型, 奇數為提領, 偶數為存款
+    /// </summary>
+    public enum LogType
+    {
+        /// <summary>
+        /// 提領
+        /// </summary>
+        Withdrawal = 1,
+
+        /// <summary>
+        /// 存款
+        /// </summary>
+        Deposit = 2,
+    }
+}

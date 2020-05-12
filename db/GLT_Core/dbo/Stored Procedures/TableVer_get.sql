9cafa05c8b8f4b7dde6d18e60c52b5ab979352cc
Yushu Hsiao <yushuhsiao@gmail.com>
2020-05-12 18:00:15 +0800

    sql schema : TableVer_get, TableVer_set

---
+++ db/GLT_Core/dbo/Stored Procedures/TableVer_get.sql
@@ -0,0 +1,9 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [TableVer_get] @db varchar(20), @table varchar(30), @index int as
+begin
+	select _ver from TableVer nolock where _db=@db and _table=@table and _index=@index
+end

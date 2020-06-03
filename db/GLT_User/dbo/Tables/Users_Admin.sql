b50b2568aaf228f237704ca148916708f30f520a
Yushu Hsiao <yushuhsiao@gmail.com>
2020-06-04 04:18:35 +0800

    UserDb schema

---
+++ db/GLT_User/dbo/Tables/Users_Admin.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users_Admin] (
+    [Id]        INT NOT NULL,
+    [ManagerId] INT NULL,
+    CONSTRAINT [PK_Users_Admin] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ db/GLT_User/dbo/Tables/Users_Member.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Users_Member] (
+    [Id] INT NOT NULL,
+    CONSTRAINT [PK_Users_Member] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

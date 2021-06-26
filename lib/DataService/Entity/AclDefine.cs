a450b36115c029b39fb4f7e89a4f81fcc255c9ef
Yushu Hsiao <yushuhsiao@gmail.com>
2021-06-27 00:18:53 +0800

    .

--- lib/DataService/Entity/AclDefine.cs
+++ lib/DataService/Entity/AclDefine.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace CMS.Entity
 {
+    /// <summary>
+    /// 權限項目定義
+    /// </summary>
     [TableName("AclDefine", Database = _Consts.Database.CoreDB)]
     public class AclDefine
     {
@@ -13,5 +16,34 @@ namespace CMS.Entity
         public string Name { get; set; }
         public string FullName { get; set; }
         public AclFlags Flags { get; set; }
+    }
+
+    [TableName("AclGroup", Database = _Consts.Database.UserDB)]
+    public class AclGroup
+    {
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public int OwnerUserId { get; set; }
+    }
+
+    [TableName("AclGroupRole", Database = _Consts.Database.UserDB)]
+    public class AclGroupRole
+    {
+        public int GroupId { get; set; }
+        public int AclId { get; set; }
+    }
+
+    [TableName("AclGroupUser", Database = _Consts.Database.UserDB)]
+    public class AclGroupUser
+    {
+        public int GroupId { get; set; }
+        public int UserId { get; set; }
+    }
+
+    [TableName("AclUserRole", Database = _Consts.Database.UserDB)]
+    public class AclUserRole
+    {
+        public int AclId { get; set; }
+        public int UserId { get; set; }
     }
 }
